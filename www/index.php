91f7fc4f19bd99e44e0c2cc41adedaac5673fda9
Tom K <guywithaview@users.noreply.github.com>
2016-07-13 21:29:31 +1000

    Improved list of buttons

--- www/index.php
+++ www/index.php
@@ -4,6 +4,13 @@
 <?php
  $pageName = basename($_SERVER["SCRIPT_NAME"]);
  $pin = 0;
+ 
+ // Modify this array to add/remove buttons
+ $units = array(
+  0 => "Desk Light",
+  1 => "Coffee Machine",
+  2 => "Downstairs Light");
+ 
  if (isset($_GET["on"]))
  {
   $state = 1;
@@ -23,16 +30,14 @@
 ?>
 
 <table cellspacing='20'>
-<tr>
-<td><b>Light 1</b></td>
-<td><a href='<?php echo $pageName; ?>?off=0'><img width='100' src='off_.png' alt='off'/></a></td>
-<td><a href='<?php echo $pageName; ?>?on=0'><img width='100' src='on_.png' alt='on'/></a></td>
-</tr>
-<tr>
-<td><b>Light 2</b></td>
-<td><a href='<?php echo $pageName; ?>?off=1'><img width='100' src='off_.png' alt='off'/></a></td>
-<td><a href='<?php echo $pageName; ?>?on=1'><img width='100' src='on_.png' alt='on'/></a></td>
-</tr>
+<?php  
+foreach ($units as $k => $v)
+{
+  echo "<tr><td><b>".$v."</b></td>";
+  echo "<td><a href='".$pageName."?off=".$k."'><img width='100' src='off_.png' alt='off'/></a></td>";
+  echo "<td><a href='".$pageName."?on=".$k."'><img width='100' src='on_.png' alt='on'/></a></td></tr>";
+}
+?>
 </table>
 </body>
 </html>
