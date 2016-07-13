d80e3c0331c51b5c841de837bfd0e041ea2a26ca
Tom K <guywithaview@users.noreply.github.com>
2016-07-13 20:05:08 +1000

    Few fixes

--- www/index.php
+++ www/index.php
@@ -25,12 +25,12 @@
 <table cellspacing='20'>
 <tr>
 <td><b>Light 1</b></td>
-<td><a href='index.php?off=0'><img width='100' src='off_.png' alt='off'/></a></td>
-<td><a href='index.php?on=0'><img width='100' src='on_.png' alt='on'/></a></td>
+<td><a href='<?php echo $pageName; ?>?off=0'><img width='100' src='off_.png' alt='off'/></a></td>
+<td><a href='<?php echo $pageName; ?>?on=0'><img width='100' src='on_.png' alt='on'/></a></td>
 </tr>
 <tr>
 <td><b>Light 2</b></td>
-<td><a href='<?php echo $pageName; ?>?off=1'><img width='100' src='off_.png' alt='off'/></a></t$
+<td><a href='<?php echo $pageName; ?>?off=1'><img width='100' src='off_.png' alt='off'/></a></td>
 <td><a href='<?php echo $pageName; ?>?on=1'><img width='100' src='on_.png' alt='on'/></a></td>
 </tr>
 </table>
